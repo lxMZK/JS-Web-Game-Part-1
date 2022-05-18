738b6a878e7b3d3f627764fc96d13f0d0939eca4
Alex Mizak <alexmmizak@gmail.com>
2022-05-18 19:38:32 -0400

    Added character movement

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
 let item = 11
+let speed = 10
+
+// Character Movement
+document.onkeydown = keyDown
+document.onkeyup = keyUp
+function keyDown(e) {
+    switch (e.keyCode) {
+        case 37:
+            // Move Left
+            greenCharacter.src = 'assets/green-character/west.gif'
+            if (parseInt(greenCharacter.style.left) - speed > 0) {
+                greenCharacter.style.left = (parseInt(greenCharacter.style.left) - speed) + 'px'
+            }
+            break;
+        case 38:
+            // Move Up
+            greenCharacter.src = 'assets/green-character/north.gif'
+            if (parseInt(greenCharacter.style.bottom) + speed < 500) {
+                greenCharacter.style.bottom = (parseInt(greenCharacter.style.bottom) + speed) + 'px'
+            }
+            break;
+        case 39:
+            // Move Right
+            greenCharacter.src = 'assets/green-character/east.gif'
+            if (parseInt(greenCharacter.style.left) + speed < (window.innerWidth - greenCharacter.width)) {
+                greenCharacter.style.left = (parseInt(greenCharacter.style.left) + speed) + 'px'
+            }
+            break;
+        case 40:
+            // Move Down
+            greenCharacter.src = 'assets/green-character/south.gif'
+            if (parseInt(greenCharacter.style.bottom) - speed > 0) {
+                greenCharacter.style.bottom = (parseInt(greenCharacter.style.bottom) - speed) + 'px'
+            }
+            break;
+        case 16:
+            // Shift Key
+            speed = 20
+            break;
+        default:
+            console.log(e.keyCode)
+            break;
+    }
+}
+
+function keyUp(e) {
+    if (e.keyCode == 16) {
+        speed = 10
+    }
+}
 
 function newImage(src, left, bottom) {
     let img = document.createElement('img')
@@ -7,6 +57,7 @@ function newImage(src, left, bottom) {
     img.style.left = left
     img.style.bottom = bottom
     document.body.append(img)
+    return img
 }
 
 function newItem(src, left, bottom) {
@@ -18,6 +69,7 @@ function newItem(src, left, bottom) {
     img.style.cursor = 'pointer'
     document.body.append(img)
 
+    // Functionality for inventory
     img.addEventListener('dblclick', function () {
         if (img.style.top != '11px') {
             img.style.left = item + 'px'
@@ -43,7 +95,7 @@ for (let y = 500; y <= window.innerHeight; y += 100) {
 
 // Renders images
 newImage('inv.png')
-newImage('green-character.gif', '100px', '100px')
+let greenCharacter = newImage('green-character.gif', '100px', '100px')
 newImage('pine-tree.png', '450px', '200px')
 newImage('tree.png', '200px', '300px')
 newImage('pillar.png', '350px', '100px')
